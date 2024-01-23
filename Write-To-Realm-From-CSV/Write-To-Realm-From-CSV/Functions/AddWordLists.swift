3004984205e625ac82aab574ffcbfd6a4475e00c
scottohalloran <o_halloran@yahoo.com>
2024-01-23 12:06:35 -0800

    Added Add Words button

---
+++ Write-To-Realm-From-CSV/Write-To-Realm-From-CSV/Functions/AddWordLists.swift
@@ -0,0 +1,8 @@
+//
+//  AddWordLists.swift
+//  Write-To-Realm-From-CSV
+//
+//  Created by Scott O'Halloran on 8/27/23.
+//
+
+import Foundation
---
+++ Write-To-Realm-From-CSV/Write-To-Realm-From-CSV/Functions/AddWordsToTextFile.swift
@@ -0,0 +1,8 @@
+//
+//  AddWordsToTextFile.swift
+//  Write-To-Realm-From-CSV
+//
+//  Created by Scott O'Halloran on 1/21/24.
+//
+
+import Foundation
