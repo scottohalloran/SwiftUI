3f2d96149a18d423bb186473cbba34b73e889c4a
Scott O'Halloran <o_hal>
2025-10-18 09:25:47 -0700

    18OCT25 Commit1

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Scott O'Halloran on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
